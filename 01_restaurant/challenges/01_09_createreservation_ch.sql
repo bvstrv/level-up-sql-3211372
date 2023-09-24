a914cba286f28d76a60b1f512a955da015c11212
bvstrv <122733152+bvstrv@users.noreply.github.com>
2023-09-24 17:33:38 +0000

    ex9

--- 01_restaurant/challenges/01_09_createreservation_ch.sql
+++ 01_restaurant/challenges/01_09_createreservation_ch.sql
@@ -4,3 +4,19 @@
 -- Use the following information:
 -- Sam McAdams (smac@kinetecoinc.com), for 5 people
 -- on August 12, 2022 at 6PM (18:00)
+
+INSERT INTO Customers (
+FirstName, LastName, Email
+)
+VALUES (
+'Sam', 'McAdams', 'smac@kinetecoinc.com'
+)
+;
+
+INSERT INTO Reservations 
+(CustomerID, Date, PartySize)
+VALUES
+((select CustomerID from Customers where Email = 'smac@kinetecoinc.com'),
+2018-08-12 18:00:00, 5);
+
+select * from Reservations WHERE customerid = (select CustomerID from Customers where Email = 'smac@kinetecoinc.com');
