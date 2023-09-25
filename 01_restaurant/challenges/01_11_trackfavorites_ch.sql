26eab784fa118a67641e6a40c0c74e93570d428a
bvstrv <122733152+bvstrv@users.noreply.github.com>
2023-09-25 18:43:59 +0000

    ex11 v1

--- 01_restaurant/challenges/01_11_trackfavorites_ch.sql
+++ 01_restaurant/challenges/01_11_trackfavorites_ch.sql
@@ -2,3 +2,13 @@
 
 -- Set Cleo Goldwater's favorite dish to
 -- the Quinoa Salmon Salad.
+
+-- select customerid from customers where lastname = 'Goldwater' and firstname = 'Cleo';
+
+-- select * from CustomersDishes where customerid = 42;
+
+-- select * from Dishes where Name = 'Quinoa Salmon Salad';
+
+INSERT INTO CustomersDishes
+(CustomerID, DishID) VALUES
+(42, 9);
