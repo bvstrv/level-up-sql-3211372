73f52bfa666da0bb09c8dd4648955357a2c55194
bvstrv <122733152+bvstrv@users.noreply.github.com>
2023-10-10 21:38:39 +0000

    ex 202

--- 02_library/challenges/02_01_checkavailability_ch.sql
+++ 02_library/challenges/02_01_checkavailability_ch.sql
@@ -1,2 +1,11 @@
 -- Determine how many copies of the book 'Dracula'
 -- are available for library patrons to borrow.
+
+SELECT (select COUNT(books.Title) from books where title = 'Dracula') -
+(select COUNT(Loans.BookID) from Loans
+INNER JOIN Books ON Loans.BookID = Books.BookID
+WHERE Books.Title = 'Dracula'
+AND ReturnedDate IS NULL)
+AS AvailableBooks;
+
+-- select * from books where title = 'Dracula';
